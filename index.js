3c62698f06adeb13839be7a22b203e398a7eea7d
Sammy Nwankwere <chidera.nwankwere@gmail.com>
2023-12-18 12:33:56 +0100

    User List Updated

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-
-  // fetch('https://reqres.in/api/users?page=1')
-   // .then(res => res.json())
-    //.then(data => {
-    //    const firstNameColumn = document.getElementById('firstNameColumn');
-       // const lastNameColumn = document.getElementById('lastNameColumn');
-     //   const emailColumn = document.getElementById('emailColumn')
-      //  const userid =document.getElementById('userid')
-
-    
-       // const user = data.data[0]; 
-
-        
-       // firstNameColumn.textContent = user.first_name+user.last_name;
-       // emailColumn.textContent = user.email;
-       // userid.textContent =user.id
-
-   // })
-  //  .catch(error => console.error(error));
-    fetch('https://reqres.in/api/users?page=1')
+fetch('https://reqres.in/api/users?page=1')
     .then(res => res.json())
     .then(data => {
+        // Iterate over the users and populate each row (assuming you have a table with tbody and tr elements)
         const tableBody = document.querySelector('tbody');
         const rows = tableBody.querySelectorAll('tr');
-
-        // Iterate over the first 6 users and populate each row
-        for (let users = 0; users < 6; users++) {
+        
+        for (let users = 0; users < Math.min(data.data.length, 6); users++) {
             const user = data.data[users];
             const cells = rows[users].querySelectorAll('td');
 
-
-            cells[0].textContent =user.id;
-            cells[1].textContent = `${user.first_name} ${ user.last_name}`
+            cells[0].textContent = user.first_name;
+            cells[1].textContent = user.last_name;
             cells[2].textContent = user.email;
-
-           
         }
+
+        // Event listener for the "Edit" buttons
+        const editButtons = document.querySelectorAll('.edit');
+
+        editButtons.forEach((editButton, index) => {
+            editButton.addEventListener('click', function() {
+                const modal = document.querySelector('.bg-modal');
+                modal.style.display = 'flex';
+
+                // Assuming you have input fields with IDs: userName, userLname, userEmail
+                const userFirstNameInput = document.getElementById('userName');
+                const userLastNameInput = document.getElementById('userLname');
+                const userEmailInput = document.getElementById('userEmail');
+
+                // Fetch the selected user's data
+                const selectedUser = data.data[index];
+
+                const modalAvatar =document.getElementById('userAvatar');
+
+                modalAvatar.src= selectedUser.avatar;
+                modalAvatar.alt=`${selectedUser.first_name}'s avatar`;
+
+                // Populate input fields with user data
+                userFirstNameInput.value = selectedUser.first_name;
+                userLastNameInput.value = selectedUser.last_name;
+                userEmailInput.value = selectedUser.email;
+            });
+        });
     })
     .catch(error => console.error(error));
 
+// Event listener for the modal close button
+document.querySelector('.close').addEventListener('click', function() {
+    const modal = document.querySelector('.bg-modal');
+    modal.style.display = 'none';
+});
--- script.js
+++ script.js
@@ -1,20 +1,71 @@
-fetch('https://reqres.in/api/users?page=2')
-.then(res => res.json())
-.then(data => {
-    const tableBody = document.querySelector('tbody');
-    const rows = tableBody.querySelectorAll('tr');
+//fetch('https://reqres.in/api/users?page=2')
+//.then(res => res.json())
+//.then(data => {
+ //   const tableBody = document.querySelector('tbody');
+ //   const rows = tableBody.querySelectorAll('tr');
 
     // Iterate over the first 6 users and populate each row
-    for (let users2 = 0; users2 < 6; users2++) {
-        const user = data.data[users2];
-        const cells = rows[users2].querySelectorAll('td');
+//    for (let users2 = 0; users2 < 6; users2++) {
+ //       const user = data.data[users2];
+//        const cells = rows[users2].querySelectorAll('td');
 
 
-        cells[0].textContent =user.id;
-        cells[1].textContent = `${user.first_name} ${ user.last_name}`
-        cells[2].textContent = user.email;
+ //       cells[0].textContent =user.id;
+ //       cells[1].textContent = `${user.first_name} ${ user.last_name}`
+//        cells[2].textContent = user.email;
 
        
-    }
-})
-.catch(error => console.error(error));
+ //   }
+//})
+//.catch(error => console.error(error));
+fetch('https://reqres.in/api/users?page=2')
+    .then(res => res.json())
+    .then(data => {
+        //  populate each row
+        const tableBody = document.querySelector('tbody');
+        const rows = tableBody.querySelectorAll('tr');
+        
+        for (let users = 0; users < Math.min(data.data.length, 6); users++) {
+            const user = data.data[users];
+            const cells = rows[users].querySelectorAll('td');
+
+            cells[0].textContent = user.first_name;
+            cells[1].textContent = user.last_name;
+            cells[2].textContent = user.email;
+        }
+
+        // Event listener for the "Edit" buttons
+        const editButtons = document.querySelectorAll('.edit');
+
+        editButtons.forEach((editButton, index) => {
+            editButton.addEventListener('click', function() {
+                const modal = document.querySelector('.bg-modal');
+                modal.style.display = 'flex';
+
+                // Assuming you have input fields with IDs: userName, userLname, userEmail
+                const userFirstNameInput = document.getElementById('userName');
+                const userLastNameInput = document.getElementById('userLname');
+                const userEmailInput = document.getElementById('userEmail');
+
+                // Fetch the selected user's data
+                const selectedUser = data.data[index];
+
+                const modalAvatar =document.getElementById('userAvatar');
+
+                modalAvatar.src= selectedUser.avatar;
+                modalAvatar.alt=`${selectedUser.first_name}'s avatar`;
+
+                // Populate input fields with user data
+                userFirstNameInput.value = selectedUser.first_name;
+                userLastNameInput.value = selectedUser.last_name;
+                userEmailInput.value = selectedUser.email;
+            });
+        });
+    })
+    .catch(error => console.error(error));
+
+// Event listener for the modal close button
+document.querySelector('.close').addEventListener('click', function() {
+    const modal = document.querySelector('.bg-modal');
+    modal.style.display = 'none';
+});
